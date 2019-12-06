c09b761985e0a47f8a78c5c3336f5d33372e596c
杨明 <1070038355@qq.com>
2019-12-06 17:16:42 +0800

    fix: 环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,54 @@
 const path = require('path');
 const HWP = require('html-webpack-plugin');
-const CWP =require('clean-webpack-plugin');
+const { CleanWebpackPlugin } =require('clean-webpack-plugin');
 module.exports = {
   entry:'./src/index.js',
   output: {
     filename: 'bundle.js',
-    path:path.resolve(_dirname,'dist')
+    path:path.resolve(__dirname,'dist')
   },
   devtool:'inline-source-map',
   plugins:[
-    new CWP(['dist']),
+    new CleanWebpackPlugin(),
     new HWP({
       title:'new pages'
     })
   ],
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use:[
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use:[
+          'xml-loader'
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase:'./dist'
   }
