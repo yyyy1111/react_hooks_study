757cac60db8119fc100dae9efc2e61e78e04cb45
杨明 <1070038355@qq.com>
2020-07-23 17:49:04 +0800

    FIX: 添加

---
+++ theApp/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+  res.send('HELLO WORLD!');
+});
+var server = app.listen(3000,function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('eg app listening at http://%s:%s', host,port);
+})
