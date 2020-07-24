657abf93b78c72fbb9b4903b269520d493155af9
杨明 <1070038355@qq.com>
2020-07-24 11:34:09 +0800

    FIX: node

---
+++ Demos/macNpm.md
@@ -0,0 +1,2 @@
+### 命令行使用技巧
+* 下载某个包遇到“Error: EACCES: permission denied, access '/usr/local/lib/node_modules/express-generator'”的问题时，在下载的包前面加“sudo” 指令即开启管理员权限下载。
---
+++ myApp/routes/register.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+router.post('/',function(req,res){
+  res.send('我是register')
+});
+connection.end();
+module.exports = router;
